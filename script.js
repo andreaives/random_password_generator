178688c656d55cfd3e3fb918e31d25788836e9f9
andreaives <andreasue3@gmail.com>
2020-09-25 16:40:23 -0600

    Refactored code to use prompt inputs

--- script.js
+++ script.js
@@ -15,44 +15,75 @@ specialChar= ["!","#","$", "%", "&"]
 function criteriaNeeded(){ 
     userLength=parseInt(prompt("How long do you want your password to be? Please choose between 8-128."));
       if (userLength >= 8 || passwordLength <= 128){
-        var userUpLetter=confirm("Would you like to use upper case letters?"); 
+        var userUpLetter=confirm("Would you like to use upper case letters?");}
         var userLowLetters=confirm("Would you like to lower case letters?");
         var userNumbers=confirm("Would you like to use numbers?");
-        var userSpecialChar=confirm("Would you like to use special characters?");
-          // generatePassword(); 
-      }
+        var userSpecialChar=confirm("Would you like to use special characters?"); 
+      
+      // else{
+      //   return 
+      // }
       
 
       if (!userLowLetters && !userUpLetter && !userNumbers && !userSpecialChar){
         alert("You must choose at least one password criteria.");
-        userUpLetter=confirm("Would you like to use upper case letters?");} 
-        userLowLetters=confirm("Would you like to lower case letters?");
-        userNumbers=confirm("Would you like to use numbers?");
-        userSpecialChar=confirm("Would you like to use special characters?");
       }
-        
-        
-      
+      else{
+        return
+      }
+      // Object to hold key value pairs
+      var passwordOptions = {
+        userLowLetters : userLowLetters,
+        userLength : userLength,
+        userNumbers : userNumbers,
+        userSpecialChar : userSpecialChar,
+        userUpLetter : userUpLetter,
+      }
+      return passwordOptions
+    }
+
+    function getRandom(arr) {
+      var randIndex = Math.floor(Math.random() * arr.length);
+      return arr[randIndex]
+    }
+    
       
     // --------generate Password------
       function generatePassword(){
-        writePassword();
+        var result = []
+        var possibleCharacters = []
+        var guaranteedCharacters = []
+        var options = criteriaNeeded()
+
+
+        if (options.userLowLetters) {
+          possibleCharacters = possibleCharacters.concat(lowLetters)
+          guaranteedCharacters.push(getRandom(lowLetters))
+        }
+        if (options.userNumbers) {
+          possibleCharacters = possibleCharacters.concat(numbers)
+          guaranteedCharacters.push(getRandom(numbers))
+        }
+        if (options.userUpLetter) {
+          possibleCharacters = possibleCharacters.concat(upLetters)
+          guaranteedCharacters.push(getRandom(upLetters))
+        }
+        if (options.userSpecialChar) {
+          possibleCharacters = possibleCharacters.concat(specialChar)
+          guaranteedCharacters.push(getRandom(specialChar))
+        }
+        
+
+        for (var i=0; i < options.userLength; i++) {
+          result.push(getRandom(possibleCharacters))
+        }
+        for (var i=0; i < guaranteedCharacters.length; i++){
+          result[i] = guaranteedCharacters[i]
+        }
+        return result.join("")
       }
-      function writePassword(){
-
-      }
-
-
-    //------- execution-----
-      console.log(userLength)
-      console.log(userLowLetters)
-      console.log(userNumbers)
-      console.log(userSpecialChar)
-      console.log(userUpLetter)
-      console.log(criteriaNeeded)
       
-         
-criteriaNeeded();
+  
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
