4cac22eba79d8395934de9137ee7a79f0a88fe46
andreaives <andreasue3@gmail.com>
2020-09-25 14:50:32 -0600

    changed the if to a while

--- script.js
+++ script.js
@@ -12,25 +12,28 @@ var upLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
 "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-numbers =["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9]
 specialChar= ["!","#","$", "%", "&"]
 
 //--------- prompt users to confirm what password criteria they would like to use--------
-function dataNeeded(){ 
+function criteriaNeeded(){ 
     userLength=parseInt(prompt("How long do you want your password to be? Please choose between 8-128."));
-      if (userLength >= 8 && passwordLength <= 128){
-        userUpLetter=confirm("Would you like to use upper case letters?");
+      if (userLength >= 8 || passwordLength <= 128){
+        userUpLetter=confirm("Would you like to use upper case letters?");}    
         userLowLetters=confirm("Would you like to lower case letters?");
         userNumbers=confirm("Would you like to use numbers?");
         userSpecialChar=confirm("Would you like to use special characters?");
-          generatePassword();
-      }
-        while (userLowLetters === false && userUpLetters === false && userNumbers === false && userSpecialChar === false){
-            alert("You must choose at least one password criteria.")
-            
-          }
+          // generatePassword(); 
 
+      while (!userLowLetters && !userUpLetter && !userNumbers && !userSpecialChar){
+        alert("You must choose at least one password criteria.")
+        dataNeeded();
+            
       }
+        
+      }  
+      
+      
     // --------generate Password------
       function generatePassword(){
         writePassword();
@@ -48,4 +51,15 @@ function dataNeeded(){
       console.log(userUpLetter)
       
          
-dataNeeded();
+criteriaNeeded();
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
