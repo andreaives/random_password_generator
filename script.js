be8a0a1416444818cd9a01446c339f25cbb6255c
andreaives <andreasue3@gmail.com>
2020-09-25 10:52:42 -0600

    Updating prompts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // declare global variables
-var userLength = "";
+
+var userLength = 0;
 var userSpecialChar;
 var userNumbers;
 var userUpLetter;
@@ -15,34 +16,28 @@ numbers =["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 specialChar= ["!","#","$", "%", "&"]
 
 //--------- prompt users to confirm what password criteria they would like to use--------
-
 function dataNeeded(){ 
-    var userLength = prompt("How long do you want your password to be? Please choose between 8-128.");
+    userLength=parseInt(prompt("How long do you want your password to be? Please choose between 8-128."));
       if (userLength >= 8 && passwordLength <= 128){
-        userUpLetter = confirm("Would you like to use upper case letters?")
-        userLowLetters = confirm("Would you like to lower case letters?")
-        userNumbers = confirm("Would you like to use numbers?" )
-        userSpecialChar =confirm("Would you like to use special characters?")
-          while (userLowLetters === false && userUpLetters === false && userNumbers === false && userSpecialChar === false){
+        userUpLetter=confirm("Would you like to use upper case letters?");
+        userLowLetters=confirm("Would you like to lower case letters?");
+        userNumbers=confirm("Would you like to use numbers?");
+        userSpecialChar=confirm("Would you like to use special characters?");
+          generatePassword();
+      }
+        while (userLowLetters === false && userUpLetters === false && userNumbers === false && userSpecialChar === false){
             alert("You must choose at least one password criteria.")
-            dataNeeded();
+            
           }
 
-      //   var userUpLetter = prompt("What upper case letter would you like to choose?");
-      // }if (userUpLetter === upLetters){
-      //    var userLowLetters = prompt("What lower case letter would you like to choose?");
-      // }if(userLowLetters === lowLetters){
-      //    var userNumbers = prompt("What number would you like to choose? 1-9");
-      // }if(userNumbers === numbers){
-      //     var userSpecialChar = prompt("what special character would you like to choose?Out of !, #, $, %, &");
-      // }if (userSpecialChar === specialChar){
-      //     alert("Awesome, generating your random password now!");
-      // } else {
-      //   prompt("Sorry, you did not choose the right characters. Please refresh the page and start over.")
-      // }
-    // --------code logic------
+      }
+    // --------generate Password------
+      function generatePassword(){
+        writePassword();
+      }
+      function writePassword(){
 
-
+      }
 
 
     //------- execution-----
@@ -52,5 +47,5 @@ function dataNeeded(){
       console.log(userSpecialChar)
       console.log(userUpLetter)
       
-      }    
+         
 dataNeeded();
